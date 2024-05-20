2664e028fe86b9cf3c6ec9db21e6cc8db225565b
Sathesh kumar <satheshs.sat@gmail.com>
2024-05-20 16:37:40 +0530

    removed cluster

--- index.js
+++ index.js
@@ -7,23 +7,23 @@ require("dotenv").config();
 require('./config/db');
 const cookieparser = require('cookie-parser');
 const tokenMiddleware = require('./middleware/token');
-const cluster = require('cluster');
-const os = require('os');
-const numCPUs = os.cpus().length;
+// const cluster = require('cluster');
+// const os = require('os');
+// const numCPUs = os.cpus().length;
 
-if (cluster.isMaster) {
-  console.log(`Master process ${process.pid} is running`);
+// if (cluster.isMaster) {
+//   console.log(`Master process ${process.pid} is running`);
 
-  for (let i = 0; i < numCPUs; i++) {
-    cluster.fork();
-  }
+//   for (let i = 0; i < numCPUs; i++) {
+//     cluster.fork();
+//   }
 
-  cluster.on('exit', (worker, code, signal) => {
-    console.log(`Worker process ${worker.process.pid} died. Restarting...`);
-    cluster.fork();
-  });
-  return;
-}
+//   cluster.on('exit', (worker, code, signal) => {
+//     console.log(`Worker process ${worker.process.pid} died. Restarting...`);
+//     cluster.fork();
+//   });
+//   return;
+// }
 
 const app = express();
 
